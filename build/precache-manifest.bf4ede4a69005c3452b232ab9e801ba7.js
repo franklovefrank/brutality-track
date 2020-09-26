c2439f035c9026bd4c1eccf802bc75cbb2aae2b4
Anna Claire <acfields11@gmail.com>
2020-09-25 20:07:11 -0500

    first

---
+++ build/precache-manifest.bf4ede4a69005c3452b232ab9e801ba7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed78fc887a7115498ec7858cb05d3b5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3c84abaefb61befe9ba",
+    "url": "/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "f17c16396996af350c28",
+    "url": "/static/css/main.732a7397.chunk.css"
+  },
+  {
+    "revision": "c3c84abaefb61befe9ba",
+    "url": "/static/js/2.0e6ed597.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0e6ed597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f17c16396996af350c28",
+    "url": "/static/js/main.8adb70e7.chunk.js"
+  },
+  {
+    "revision": "c7cf4515ba8e6075611f",
+    "url": "/static/js/runtime-main.c10c927b.js"
+  }
+]);
